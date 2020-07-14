c302d2b21930aee0620814a6eac1a83fd1035746
Josh Nussbaum <Joshnussbaum89@gmail.com>
2020-07-14 10:41:48 -0400

    sticky nav issue

--- js/main.js
+++ js/main.js
@@ -5,4 +5,17 @@ $("button").click(function () {
 
 //ADD STICKY NAV
 
+const mq = window.matchMedia("(min-width: 768px)");
+
+function screenSize(e) {
+    if (e.matches) {
+        $('.main-nav').sticky();
+    }
+    else {
+        $(".main-nav").unstick();
+    }
+}
+
+mq.addListener(screenSize);
+
 //ADD DYNAMIC SCROLLING, EITHER TRANSITIONS OR DIFFERENT SCROLLBAR
